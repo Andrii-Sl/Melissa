8bb7bf8c2a7a41b67caff0a5d79fdb145c3cca11
Andrii-Sl <slynko.andrey@gmail.com>
2026-04-29 18:32:13 +0200

    Update auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,9 +1,5 @@
 export function verifyCode(phone: string, code: string) {
-  // спец-вход без SMS
   if (phone === "140578") return true;
-
-  // тестовый код
   if (code === "1234") return true;
-
   return false;
 }
