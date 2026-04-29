3673470d9ecb7b28f42cb67471bf4640dcf1c12d
Andrii-Sl <slynko.andrey@gmail.com>
2026-04-29 23:49:47 +0200

    Update auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,17 @@
-export function verifyCode(phone: string, code: string) {
-  if (phone === "140578") return true;
-  if (code === "1234") return true;
-  return false;
+export function verifyLogin(input: string) {
+  if (input === "140578") {
+    return {
+      user: "Андрей",
+      token: "fake-jwt"
+    };
+  }
+
+  if (input.startsWith("+")) {
+    return {
+      user: "User",
+      token: "sms-jwt"
+    };
+  }
+
+  return null;
 }
