3e096ce741fd146958a66d314bdeaa47ee7f9029
EU parts <slynko.andrey@gmail.com>
2026-05-01 01:19:07 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,14 @@
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Benefits from "../components/Benefits";
-import HowItWorks from "../components/HowItWorks";
 import Footer from "../components/Footer";
 
-export default function HomePage() {
+export default function Home() {
   return (
     <main>
       <Header />
       <Hero />
       <Benefits />
-      <HowItWorks />
       <Footer />
     </main>
   );
