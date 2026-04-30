18be31311cae75be33856075b78a6c6637f373ea
EU parts <slynko.andrey@gmail.com>
2026-05-01 01:22:06 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,5 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextResponse } from "next/server";
 
-export function middleware(req: NextRequest) {
-  const token = req.cookies.get('auth')?.value
-  if (req.nextUrl.pathname.startsWith('/dashboard') && !token) {
-    return NextResponse.redirect(new URL('/login', req.url))
-  }
-  return NextResponse.next()
+export function middleware() {
+  return NextResponse.next();
 }
-
-export const config = {
-  matcher: ['/dashboard/:path*']
-  }
