dc54b4a3a365a702b34f57fd9d658fba19ce8b9a
EU parts <slynko.andrey@gmail.com>
2026-04-30 21:57:02 +0200

    Update admin.ts

--- lib/admin.ts
+++ lib/admin.ts
@@ -1,5 +1,5 @@
-import { createClient } from '@supabase/supabase-js'
-export const admin = createClient(
- process.env.NEXT_PUBLIC_SUPABASE_URL!,
- process.env.SUPABASE_SERVICE_ROLE_KEY!
-)
+export const admin = {
+  async getOrders() {
+    return [];
+  }
+};
