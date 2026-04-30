46c3a517677e8498172d9ac10e644a6ba071b66e
EU parts <slynko.andrey@gmail.com>
2026-04-30 21:57:58 +0200

    Update auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,17 +1,3 @@
-export function verifyLogin(input: string) {
-  if (input === "140578") {
-    return {
-      user: "Андрей",
-      token: "fake-jwt"
-    };
-  }
-
-  if (input.startsWith("+")) {
-    return {
-      user: "User",
-      token: "sms-jwt"
-    };
-  }
-
-  return null;
+export function checkLogin(code: string) {
+  return code === "140578";
 }
