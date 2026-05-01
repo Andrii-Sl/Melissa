74401ec98e23e7c74d2956b86d256e7fd930912c
EU parts <slynko.andrey@gmail.com>
2026-05-01 02:56:45 +0200

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,26 +1,37 @@
 export default function Hero() {
   return (
     <section className="hero">
-      <div className="heroLeft">
-        <p className="badge">Премиум маркетплейс Европы</p>
+      <div className="left">
+        <p className="badge">Оригинальные детали из Европы</p>
 
         <h1>
           Подбор автозапчастей
           <br />
-          по VIN коду
+          по VIN номеру
         </h1>
 
         <p className="sub">
           BMW • Audi • Mercedes • VW • Skoda
         </p>
 
-        <input placeholder="Введите VIN номер..." className="vinInput" />
+        <input
+          className="vin"
+          placeholder="Введите VIN код автомобиля..."
+        />
 
-        <button className="cta">Подобрать детали</button>
+        <button className="cta">
+          Подобрать детали
+        </button>
+
+        <div className="trust">
+          <span>✔ Доставка в Казахстан</span>
+          <span>✔ Гарантия</span>
+          <span>✔ Быстрый ответ</span>
+        </div>
       </div>
 
-      <div className="heroRight">
-        <img src="/audi.jpg" alt="Audi" className="heroCar" />
+      <div className="right">
+        <img src="/audi.jpg" alt="Audi" className="car" />
       </div>
     </section>
   );
