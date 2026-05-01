3956923484cc9bab3788ab6b4495d2c1e3f25f78
EU parts <slynko.andrey@gmail.com>
2026-05-01 03:54:54 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
-import Header from "../components/Header";
-import Hero from "../components/Hero";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import Footer from "@/components/Footer";
 
-export default function Home() {
+export default function HomePage() {
   return (
     <main>
       <Header />
       <Hero />
+      <Footer />
     </main>
   );
 }
