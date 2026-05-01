78cd40e123a24222004a698c2a7b2303a9c5a3b2
EU parts <slynko.andrey@gmail.com>
2026-05-01 23:05:39 +0200

    Create users.ts

---
+++ data/users.ts
@@ -0,0 +1,20 @@
+export const users = [
+  {
+    email: "admin@mail.com",
+    password: "1234",
+    role: "admin",
+    name: "Владелец"
+  },
+  {
+    email: "client@mail.com",
+    password: "1234",
+    role: "client",
+    name: "Андрей"
+  },
+  {
+    email: "supplier@mail.com",
+    password: "1234",
+    role: "supplier",
+    name: "Dostawca"
+  }
+];
