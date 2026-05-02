f7d828430d1c57f82aaa50f1aebfed807074a2d9
EU parts <slynko.andrey@gmail.com>
2026-05-02 17:10:15 +0200

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,58 @@
+import styles from "./footer.module.css";
+
 export default function Footer() {
   return (
-    <footer className="footer">
-      <p>© 2026 AutoParts EU</p>
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+
+        <div className={styles.top}>
+
+          <div>
+            <img
+              src="/logo-final.png"
+              alt="AutoParts EU"
+              className={styles.logo}
+            />
+
+            <p className={styles.text}>
+              Поставка автозапчастей из Европы.
+              Оригинальные детали и качественные аналоги.
+            </p>
+          </div>
+
+          <div className={styles.col}>
+            <h4>Навигация</h4>
+            <a href="/">Главная</a>
+            <a href="/about">О компании</a>
+            <a href="/how-it-works">
+              Как работает сервис
+            </a>
+            <a href="/schedule">
+              Расписание поставок
+            </a>
+            <a href="/contacts">
+              Контакты
+            </a>
+          </div>
+
+          <div className={styles.col}>
+            <h4>Контакты</h4>
+            <a href="tel:+490000000000">
+              +49 000 000 0000
+            </a>
+            <a href="mailto:info@autoparts-eu.com">
+              info@autoparts-eu.com
+            </a>
+            <span>Пн–Пт: 09:00–18:00</span>
+          </div>
+
+        </div>
+
+        <div className={styles.bottom}>
+          © 2026 AutoParts EU. All rights reserved.
+        </div>
+
+      </div>
     </footer>
   );
 }
