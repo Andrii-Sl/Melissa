9ae150d2c494f289bfe92f98c7edd74ad428590d
EU parts <slynko.andrey@gmail.com>
2026-05-05 02:58:16 +0200

    Update Menu.tsx

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -6,6 +6,8 @@ import styles from "./menu.module.css";
 export default function Menu() {
   const [open, setOpen] = useState(false);
 
+  const close = () => setOpen(false);
+
   return (
     <>
       {/* BURGER */}
@@ -19,31 +21,35 @@ export default function Menu() {
       {/* OVERLAY */}
       <div
         className={`${styles.overlay} ${open ? styles.show : ""}`}
-        onClick={() => setOpen(false)}
+        onClick={close}
       />
 
-      {/* MENU */}
+      {/* POPUP */}
       <div
         className={`${styles.popup} ${open ? styles.open : ""}`}
+        onClick={(e) => e.stopPropagation()}
       >
-        <button
-          className={styles.close}
-          onClick={() => setOpen(false)}
-        >
+        <button className={styles.close} onClick={close}>
           ✕
         </button>
 
         <nav className={styles.nav}>
-          <a href="/about" onClick={() => setOpen(false)}>О компании</a>
-          <a href="/how-it-works" onClick={() => setOpen(false)}>Как работает</a>
-          <a href="/schedule" onClick={() => setOpen(false)}>Поставки</a>
-          <a href="/contacts" onClick={() => setOpen(false)}>Контакты</a>
+          <a href="/about" onClick={close}>О компании</a>
+          <a href="/how-it-works" onClick={close}>Как работает</a>
+          <a href="/schedule" onClick={close}>Поставки</a>
+          <a href="/contacts" onClick={close}>Контакты</a>
         </nav>
 
         <div className={styles.lang}>
-          <div className={styles.langTitle}>Language</div>
-          <a href="/" onClick={() => setOpen(false)}>🇷🇺 Русский</a>
-          <a href="/en" onClick={() => setOpen(false)}>🇬🇧 English</a>
+          <span className={styles.langTitle}>Language</span>
+
+          <a href="/" onClick={close}>
+            🇷🇺 Русский
+          </a>
+
+          <a href="/en" onClick={close}>
+            🇬🇧 English
+          </a>
         </div>
       </div>
     </>
