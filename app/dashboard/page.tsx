34cd72ad98ebf35eda79f0811cd327f9f38173ce
EU parts <slynko.andrey@gmail.com>
2026-05-09 00:23:46 +0200

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { supabase } from "@/lib/supabase";
-import styles from "../dashboard.module.css";
+import styles from "./dashboard.module.css";
 
 export default function ProfilePage() {
 
