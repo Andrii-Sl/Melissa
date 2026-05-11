ce789d3b981ef5d569bf29562ff8bb764b5d6b11
EU parts <slynko.andrey@gmail.com>
2026-05-12 00:55:16 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+
+  images: {
+
+    remotePatterns: [
+
+      {
+        protocol: "https",
+        hostname: "**.supabase.co",
+      },
+
+    ],
+
+  },
+
+};
 
 module.exports = nextConfig;
