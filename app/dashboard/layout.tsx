c6246f19eff7adeccf325fa7bf3481d163ee1594
EU parts <slynko.andrey@gmail.com>
2026-05-13 22:15:11 +0200

    Update layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,24 +1,8 @@
-"use client";
-
-import BottomNav from "@/components/BottomNav";
-import TopBar from "@/components/TopBar";
-
 export default function DashboardLayout({
   children,
 }:{
-  children:React.ReactNode;
+  children: React.ReactNode;
 }) {
 
-  return (
-
-    <>
-
-      <TopBar />
-
-      {children}
-
-
-    </>
-
-  );
+  return children;
 }
